bcab0ee23a0763d685072470facffe8cb53ba64c
SebAvenel <sebastien.avenel@outlook.fr>
2020-07-30 17:54:14 +0200

    création d'une classe pour la création d'un widget de réseaux sociaux

---
+++ wp-content/themes/seb_Immo/inc/widgets/social.php
@@ -0,0 +1,11 @@
+<?php
+
+class SebImmo_Social_widget extends WP_Widget {
+
+    public function __construct()
+    {
+        parent::__construct('seb_immo_social_widget', __('Social widget', 'seb_Immo'));
+    }
+
+
+}
