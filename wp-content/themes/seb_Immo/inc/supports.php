ca68df7bd0e92376d38cf2a12fa181d24c70d1df
SebAvenel <sebastien.avenel@outlook.fr>
2020-07-30 15:42:13 +0200

    ajout d'un filtre pour l'autorisation de téléverser des images au format svg

--- wp-content/themes/seb_Immo/inc/supports.php
+++ wp-content/themes/seb_Immo/inc/supports.php
@@ -8,3 +8,8 @@ add_action('after_setup_theme', function (){
     add_theme_support('post-thumbnails');
 });
 
+add_filter('upload_mimes', function ($mimes){
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+});
+
