7a76845a62634bc81f1d8e476056d21e580ebd40
SebAvenel <sebastien.avenel@outlook.fr>
2020-07-30 14:29:25 +0200

    ajout des supports

--- wp-content/themes/seb_Immo/functions.php
+++ wp-content/themes/seb_Immo/functions.php
@@ -1,1 +1,3 @@
 <?php
+
+require_once('inc/supports.php');
--- wp-content/themes/seb_Immo/inc/supports.php
+++ wp-content/themes/seb_Immo/inc/supports.php
@@ -1,3 +1,10 @@
 <?php
 define('ABSPATH') or die('');
 
+add_action('after_setup_theme', function (){
+    add_theme_support('title-tag');
+    add_theme_support('menus');
+    add_theme_support('html5');
+    add_theme_support('post-thumbnails');
+});
+
