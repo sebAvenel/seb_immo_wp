e76ba8056def07695150b54c9122210cdac9ed7d
SebAvenel <sebastien.avenel@outlook.fr>
2020-07-30 18:09:53 +0200

    création de la méthode qui retournera le contenu html du widget

--- wp-content/themes/seb_Immo/inc/widgets/social.php
+++ wp-content/themes/seb_Immo/inc/widgets/social.php
@@ -7,5 +7,20 @@ class SebImmo_Social_widget extends WP_Widget {
         parent::__construct('seb_immo_social_widget', __('Social widget', 'seb_Immo'));
     }
 
+    /**
+     * Retourne le contenu html du widget
+     * @param array $args
+     * @param array $instance
+     */
+    public function widget($args, $instance)
+    {
+        echo $args['before_widget'];
+        if (isset($instance['title'])){
+            $title = apply_filters('widget_title', $instance['title']);
+            echo $args['before_title'] . $title . $args['after_title'];
+        }
+        echo 'Réseaux sociaux !!';
+        echo $args['after_widget'];
+    }
 
 }
