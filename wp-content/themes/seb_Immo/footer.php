f36ede4425fd1701dcb0fe4d454768462100d6bb
SebAvenel <sebastien.avenel@outlook.fr>
2020-07-30 23:11:18 +0200

    affichage des réesaux sociaux

--- wp-content/themes/seb_Immo/footer.php
+++ wp-content/themes/seb_Immo/footer.php
@@ -26,24 +26,6 @@
         </ul>
     </div>
     <div class="footer__col">
-        <div class="footer__credits">© 2020 Seb Immo Real Estate, Inc.</div>
-        <div class="footer__social">
-            <a href="#" title="Nous suivre sur twitter">
-                <svg class="icon">
-                    <use xlink:href="sprite.14d9fd56.svg#twitter"></use>
-                </svg>
-            </a>
-            <a href="#" title="Nous suivre sur Facebook">
-                <svg class="icon">
-                    <use xlink:href="sprite.14d9fd56.svg#facebook"></use>
-                </svg>
-            </a>
-            <a href="#" title="Nous suivre sur instagram">
-                <svg class="icon">
-                    <use xlink:href="sprite.14d9fd56.svg#instagram"></use>
-                </svg>
-            </a>
-        </div>
     </div>
     -->
 </footer>
