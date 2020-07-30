77232e3423cd53188774477e69fcfd5a4d0eeaf1
SebAvenel <sebastien.avenel@outlook.fr>
2020-07-30 15:37:18 +0200

    création d'une action pour ajouter une image de logo par le hook customize

--- wp-content/themes/seb_Immo/functions.php
+++ wp-content/themes/seb_Immo/functions.php
@@ -2,3 +2,4 @@
 
 require_once('inc/supports.php');
 require_once('inc/assets.php');
+require_once('inc/apparence.php');
---
+++ wp-content/themes/seb_Immo/inc/apparence.php
@@ -0,0 +1,14 @@
+<?php
+
+add_action('customize_register', function (WP_Customize_Manager $manager){
+    $manager->add_section('seb_immo_appearance', [
+        'title' => __('Theme appearance')
+    ]);
+
+    $manager->add_setting('logo');
+
+    $manager->add_control(new WP_Customize_Image_Control($manager, 'logo', [
+        'label' => __('Logo', 'Seb Immo'),
+        'section' => 'seb_immo_appearance'
+    ]));
+});
