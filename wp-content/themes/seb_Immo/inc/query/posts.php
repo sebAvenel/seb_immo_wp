62115e2708080d80a2622d297ddd92d791e9c1d6
SebAvenel <sebastien.avenel@outlook.fr>
2020-08-24 13:16:37 +0200

    mise en place de la pagination de wordpress 2

--- wp-content/themes/seb_Immo/inc/query/posts.php
+++ wp-content/themes/seb_Immo/inc/query/posts.php
@@ -3,5 +3,5 @@ add_action('pre_get_posts', function (WP_Query $query){
     if (is_admin() || !$query->is_main_query() || !is_home()){
         return;
     }
-    $query->set('posts_per_page', 1);
+    $query->set('posts_per_page', 5);
 });
