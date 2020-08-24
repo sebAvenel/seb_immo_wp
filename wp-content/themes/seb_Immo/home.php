b0546156b3edd50e82c6f22ebba84f007e316af7
SebAvenel <sebastien.avenel@outlook.fr>
2020-08-24 12:21:06 +0200

    création d'un template pour la récupération de articles sous forme de liste (template-parts/post.php)

--- wp-content/themes/seb_Immo/home.php
+++ wp-content/themes/seb_Immo/home.php
@@ -35,34 +35,7 @@
             <div class="news-list">
                 <?php if (have_posts()): ?>
                     <?php while (have_posts()): the_post(); ?>
-                        <article class="news">
-                            <a href="<?php the_permalink() ?>" title="<?= esc_attr(get_the_title()) ?>" class="news__image">
-                            <?php if (has_post_thumbnail()): ?>
-                                <?php the_post_thumbnail() ?>
-                            <?php else: ?>
-                                <img width="250" height="250" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPcXg8AAfMBOOg8T0oAAAAASUVORK5CYII=">
-                            <?php endif; ?>
-                            </a>
-                            <div class="news__body">
-                                <header class="news__header">
-                                    <?php
-                                        $categories = get_the_category();
-                                        if (!empty($categories)):
-                                    ?>
-                                    <a class="news__tag" href="<?= get_term_link($categories[0]) ?>"><?= $categories[0]->name ?></a>
-                                    <?php endif; ?>
-                                    <a class="news__title" href="<?php the_permalink() ?>"><?= the_title() ?></a>
-                                    <div class="news__date"><?= sprintf(__('Published on %s at %s', 'seb_immo'), get_the_date(), get_the_time()) ?></div>
-                                </header>
-                                <div class="news__content">
-                                    <?php the_excerpt() ?>
-                                </div>
-                                <a href="<?php the_permalink() ?>" class="news__action">
-                                    Lire la suite
-                                    <?= seb_Immo_icon('arrow') ?>
-                                </a>
-                            </div>
-                        </article>
+                        <?php get_template_part('template-parts/post'); ?>
                     <?php endwhile; ?>
                 <?php else: ?>
                     <h2><?= __('No posts found', 'seb_Immo') ?></h2>
