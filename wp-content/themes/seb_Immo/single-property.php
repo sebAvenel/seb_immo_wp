f2226cb33c1bd9f9e302fcb59067510355ac8d16
SebAvenel <sebastien.avenel@outlook.fr>
2020-09-10 10:58:23 +0200

    création de la page single d'un bien

---
+++ wp-content/themes/seb_Immo/single-property.php
@@ -0,0 +1,90 @@
+<?php get_header(); ?>
+<div class="container">
+    <header class="bien-header">
+        <div>
+            <h1 class="bien__title">Location appartement 4 pièces de 90.23m²</h1>
+            <div class="bien__meta">
+                <div class="bien__location">Avrillé (49240)</div>
+                <div class="bien__price">838,00 €/mois</div>
+            </div>
+            <div class="bien__actions">
+                <button class="btn btn-filled">Contacter l'agence</button>
+                <button class="btn">Appeler</button>
+            </div>
+
+            <!--
+            <form action="" class="bien__form form-2column">
+              <div class="form-group">
+                <input type="text" id="username" class="form-control">
+                <label for="username">Pseudo</label>
+              </div>
+              <div class="form-group">
+                <input type="text" id="email" class="form-control">
+                <label for="email">Email</label>
+              </div>
+              <textarea placeholder="Message" class="form-control full"></textarea>
+              <button type="submit" class="btn">Commenter</button>
+            </form>
+            -->
+
+        </div>
+        <div>
+            <div class="bien__photos js-slider">
+
+                <a href="https://picsum.photos/id/41/1280/720.jpg">
+                    <img class="bien__photo" src="https://picsum.photos/id/41/777/444.jpg" alt="">
+                </a>
+
+                <a href="https://picsum.photos/id/42/1280/720.jpg">
+                    <img class="bien__photo" src="https://picsum.photos/id/42/777/444.jpg" alt="">
+                </a>
+
+                <a href="https://picsum.photos/id/43/1280/720.jpg">
+                    <img class="bien__photo" src="https://picsum.photos/id/43/777/444.jpg" alt="">
+                </a>
+
+            </div>
+        </div>
+    </header>
+
+
+    <div class="bien-body">
+        <h2 class="bien-body__title">Description</h2>
+        <div class="formatted">
+            <p>
+                <strong>EXCLUSIVITÉ - LOCATION AVRILLE. GRAND APPARTEMENT 4 PIÈCES de 90,23 m²</strong> en résidence
+                récente
+                avec ascenseur. Il propose un séjour de 23 m² avec balcon de 10 m², une belle cuisine américaine
+                aménagée et
+                équipée
+                ( plaques vitrocéramiques et hotte), trois chambres, une salle d'eau et une salle de bain. Parking privé
+                en
+                sous-sol.
+            </p>
+
+            <p>
+                Le bien se trouve à proximité des commerces. On trouve des écoles maternelles et élémentaires à 2
+                minutes de
+                l'appartement .
+            </p>
+
+            <p>
+                Le loyer de <strong>838,00 euros</strong> par mois charges comprises dont 113,00 euros par mois de
+                provision
+                pour charges (soumis à la régularisation annuelle).
+            </p>
+        </div>
+    </div>
+
+    <section class="bien-options">
+        <div class="bien-option"><img src="area.78237e19.svg" alt=""> Superficie: 93m²</div>
+        <div class="bien-option"><img src="elevator-fill.117c4510.svg" alt=""> Ascenseur</div>
+        <div class="bien-option"><img src="rooms.b02e3d15.svg" alt=""> Nbr chambres: 4</div>
+        <div class="bien-option"><img src="elevator.e0bdbd67.svg" alt=""> Etage: 3</div>
+        <div class="bien-option"><img src="parking.bb37c0bc.svg" alt=""> Parking: Oui</div>
+    </section>
+
+</div>
+<?php get_footer(); ?>
+
+
