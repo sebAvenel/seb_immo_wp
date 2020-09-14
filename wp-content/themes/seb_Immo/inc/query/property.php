168df3fa17d990022ed6d5e529aac8b89116c984
SebAvenel <sebastien.avenel@outlook.fr>
2020-09-14 18:39:56 +0200

    remise de 20 biens par page

--- wp-content/themes/seb_Immo/inc/query/property.php
+++ wp-content/themes/seb_Immo/inc/query/property.php
@@ -3,5 +3,5 @@ add_action('pre_get_posts', function (WP_Query $query){
     if (is_admin() || !is_post_type_archive('property')){
         return;
     }
-    $query->set('posts_per_page', 2);
+    $query->set('posts_per_page', 20);
 });
