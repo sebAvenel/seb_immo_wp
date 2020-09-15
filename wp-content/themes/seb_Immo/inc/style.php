6f7f8b5e4a3ef09c22f85f7548f601b0ce18d45c
SebAvenel <sebastien.avenel@outlook.fr>
2020-09-15 10:35:12 +0200

    Placer l'active sur 'louer' si on se trouve sur un single bien à louer

--- wp-content/themes/seb_Immo/inc/style.php
+++ wp-content/themes/seb_Immo/inc/style.php
@@ -4,13 +4,18 @@ add_filter('next_posts_link_attributes', function (string $attrs): string {
 });
 
 /**
- * Supprimer l'active sur 'Actualités' si on ne se trouve pas sur cette page
+ * Gérer l'active du menu en fonction de la page sur laquelle on se trouve
  */
-add_filter('nav_menu_css_class', function (array $classes): array {
+add_filter('nav_menu_css_class', function (array $classes, WP_Post $item): array {
+    // Supprimer l'active sur 'Actualités' si on ne se trouve pas sur cette page
     if (is_singular('property') || is_post_type_archive('property')){
         $classes = array_filter($classes, function (string $class){
             return $class !== 'current_page_parent';
         });
     }
+    // Placer l'active sur 'louer' si on se trouve sur un single bien à louer
+    if (is_singular('property') && agence_is_rent_url($item->url)){
+        $classes[] = 'current_page_parent';
+    }
     return $classes;
-});
+}, 10, 2);
