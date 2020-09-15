9e156c02782c88bfab4d882aa776838714b95b9b
SebAvenel <sebastien.avenel@outlook.fr>
2020-09-15 10:16:44 +0200

    Supprimer l'active sur 'Actualités' si on ne se trouve pas sur cette page

--- wp-content/themes/seb_Immo/inc/style.php
+++ wp-content/themes/seb_Immo/inc/style.php
@@ -2,3 +2,15 @@
 add_filter('next_posts_link_attributes', function (string $attrs): string {
     return $attrs . ' class="btn"';
 });
+
+/**
+ * Supprimer l'active sur 'Actualités' si on ne se trouve pas sur cette page
+ */
+add_filter('nav_menu_css_class', function (array $classes): array {
+    if (is_singular('property') || is_post_type_archive('property')){
+        $classes = array_filter($classes, function (string $class){
+            return $class !== 'current_page_parent';
+        });
+    }
+    return $classes;
+});
