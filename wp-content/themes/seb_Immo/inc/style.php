bb2095e39432d6c852713c9e41dd967e6297ecc8
SebAvenel <sebastien.avenel@outlook.fr>
2020-09-15 11:01:10 +0200

    Conditions plus strictes + meilleure répartition du code

--- wp-content/themes/seb_Immo/inc/style.php
+++ wp-content/themes/seb_Immo/inc/style.php
@@ -6,25 +6,13 @@ add_filter('next_posts_link_attributes', function (string $attrs): string {
 /**
  * Gérer l'active du menu en fonction de la page sur laquelle on se trouve
  */
-add_filter('nav_menu_css_class', function (array $classes, WP_Post $item): array {
+add_filter('nav_menu_css_class', function (array $classes): array {
     // Supprimer l'active sur 'Actualités' si on ne se trouve pas sur cette page
     if (is_singular('property') || is_post_type_archive('property')){
         $classes = array_filter($classes, function (string $class){
             return $class !== 'current_page_parent';
         });
     }
-    // Placer l'active sur 'louer' ou 'acheter' selon le type de bien sur lequel on se trouve
-    if (is_singular('property')){
-        $property = get_queried_object();
-        $category = get_field('property_category', $property);
-        if ($category === 'buy'){
-            $condition = agence_is_buy_url($item->url);
-        } else {
-            $condition = agence_is_rent_url($item->url);
-        }
-        if ($condition){
-            $classes[] = 'current_page_parent';
-        }
-    }
+
     return $classes;
-}, 10, 2);
+});
